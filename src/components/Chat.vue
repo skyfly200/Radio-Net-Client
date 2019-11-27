398457c3a376123075a93d4fbf841b8d21b0cf01
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-11-26 22:16:03 -0700

    chat core functions

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -4,7 +4,10 @@
     v-divider
     v-list(three-line)
       v-list-item(v-for="m in messages")
-        v-list-item-content {{ m.name }}: {{ m.text }}
+        v-list-item-content
+          v-list-item-title {{ m.name }}
+          p {{ m.text }}
+          p {{ Date(m.timestamp) }}
         v-list-item-avatar
           v-img(:src="m.profilePicUrl")
     .input
@@ -23,13 +26,39 @@ export default Vue.extend({
       messages: []
     };
   },
+  created() {
+    this.loadMessages();
+  },
   methods: {
+    // Loads chat messages history and listens for upcoming ones.
+    loadMessages() {
+      // Create the query to load the last 12 messages and listen for new ones.
+      var query = firebase
+        .firestore()
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .limit(12);
+
+      let t = this;
+      // Start listening to the query.
+      query.onSnapshot(function(snapshot) {
+        snapshot.docChanges().forEach(function(change) {
+          if (change.type === "removed") {
+            //deleteMessage(change.doc.id);
+          } else {
+            var message = change.doc.data();
+            t.messages.push(message);
+            //displayMessage(change.doc.id, message.timestamp, message.name, message.text, message.profilePicUrl, message.imageUrl);
+          }
+        });
+      });
+    },
     send() {
       let user = this.$store.getters.user;
       let newMessage;
       if (!user) {
         newMessage = {
-          name: "anonmous",
+          name: "guest",
           text: this.message,
           profilePicUrl: "", // TO-DO default user images
           timestamp: firebase.firestore.FieldValue.serverTimestamp()
@@ -42,22 +71,11 @@ export default Vue.extend({
           timestamp: firebase.firestore.FieldValue.serverTimestamp()
         };
       }
-      this.messages.push(newMessage);
       this.message = "";
-    },
-    // Saves a new message to your Cloud Firestore database.
-    saveMessage(messageText: String) {
-      // Add a new message entry to the database.
-      let user = this.$store.getters.user;
       return firebase
         .firestore()
         .collection("messages")
-        .add({
-          name: user.displayName,
-          text: messageText,
-          profilePicUrl: user.imageURL,
-          timestamp: firebase.firestore.FieldValue.serverTimestamp()
-        })
+        .add(newMessage)
         .catch(function(error) {
           console.error("Error writing new message to database", error);
         });
