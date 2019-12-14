f469db2c43244753182880c2b7fdb98383deee80
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-12-13 21:06:19 -0700

    podcast editing basics

--- src/views/Podcast.vue
+++ src/views/Podcast.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   .podcasts
+    ImgUpload(type="podcast" @done="updateImg($event)" @close="imageDialog = false" :toggle="imageDialog" :multi="false")
     v-row(justify="start").ml-3
       v-btn(to="/podcasts" text).mt-2 Back to Podcasts
     v-card.ma-3
@@ -23,10 +24,10 @@
                     v-icon mdi-rss
                   audio(src="''" ref="player")
             v-divider.ma-3
-            div(v-if="items.length === 0")
+            div(v-if="items === null")
               h3 Loading
               v-progress-circular(indeterminate large)
-            v-list(v-else-if="items.length > 0")
+            v-list(v-else-if="items && items.length > 0")
               v-list-item(v-for="(item, index) in items" two-line)
                 v-list-item-content
                   v-list-item-title {{ item.xml.title }}
@@ -39,14 +40,25 @@
             div(v-else)
               h1 Podcast has no items
         template(v-else-if="mode === 'edit'")
-          v-card-title Edit Podcasts
+          v-card-title Edit Podcast
           v-card-text
             v-form
-              v-text-field
-              v-text-field
-              v-text-field
-              v-text-field
-              v-btn(@click.stop="") Save
+              v-row
+                v-col(cols="6")
+                  ImgEditHover(:editable="true" v-on:open="openImageDialog()" :src="podcast.xml.image.url").image-edit
+                v-col(cols="6")
+                  v-text-field(label="Title" v-model="podcast.xml.title")
+                  v-text-field(label="Description" v-model="podcast.xml.description")
+                  v-text-field(label="Copyright" v-model="podcast.xml.copyright")
+              v-row
+                v-col
+                  h3 iTunes Fields
+                  v-text-field(label="Author" v-model="podcast.xml['itunes:author']")
+                  v-text-field(label="Keywords" v-model="podcast.xml['itunes:keywords']")
+                  v-text-field(label="Subtitle" v-model="podcast.xml['itunes:subtitle']")
+                  v-text-field(label="Summary" v-model="podcast.xml['itunes:summary']")
+                  v-text-field(label="Explicit" v-model="podcast.xml['itunes:explicit']")
+                  v-btn(@click.stop="updatePodcast()") Save
         template(v-else-if="mode === 'add'")
           v-card-title Add an item
           v-card-text
@@ -67,14 +79,19 @@ import Vue from "vue";
 import { format } from "date-fns";
 import firebase, { firestore } from "firebase";
 
+import ImgUpload from "@/components/ImgUpload.vue";
+import ImgEditHover from "@/components/ImgEditHover.vue";
+
 var db = firebase.firestore();
 
 export default Vue.extend({
+  components: { ImgEditHover, ImgUpload },
   data: () => ({
     mode: "view",
     id: "",
+    imageDialog: false,
     podcast: {},
-    items: [],
+    items: null,
     play: -1,
     src: null,
     playerState: false
@@ -93,10 +110,29 @@ export default Vue.extend({
       this.loadPodcast();
       this.loadItems();
     },
+    openImageDialog() {
+      this.imageDialog = true;
+    },
+    updateImg(image) {
+      this.imageDialog = false;
+      this.podcast.xml.image.url = image;
+    },
+    updatePodcast() {
+      // write podcast updates to firestore
+      db.collection("podcasts")
+        .doc(this.id)
+        .update(this.podcast)
+        .then(function() {
+          this.mode = "view";
+        }.bind(this))
+        .catch(function(error) {
+          console.error("Error updating podcast: ", error);
+        });
+    },
     playItem(index) {
       this.play = index;
-      // TO-DO load all items into playlist and skip to index, so playback continues through items when done
-      // Auto stop stream player on play
+      // TO-DO play loads all items into playlist and skip to index, so playback continues through items when done
+      // change to set on the master player component
       if (
         this.items[index] &&
         this.items[index].xml &&
@@ -132,10 +168,11 @@ export default Vue.extend({
               function(doc) {
                 var data = doc.data();
                 data.id = doc.id;
+                if (this.items === null) this.items = [];
                 this.items.push(data);
               }.bind(this)
             );
-            if (this.items === []) this.items = null;
+            if (querySnapshot.docs.length === 0) this.items = [];
           }.bind(this)
         );
     },
@@ -206,4 +243,7 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.image-edit
+  width: 100%
+</style>
